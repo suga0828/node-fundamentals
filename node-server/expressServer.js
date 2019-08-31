4b2964b41f0a7cd5bdc51281f433cf7c7113567f
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-08-31 09:42:44 -0500

    Execute a command (callback based)  with monitor main file

--- node-server/expressServer.js
+++ node-server/expressServer.js
@@ -1,16 +1,26 @@
 import express from 'express';
+// Built in library
+import { exec } from 'child_process';
 
 // Express server
 const server = express();
 
 // Events
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   res.send({ message: `Hello Express` });
 });
 
-server.get("/favicon.*", (req, res) => {
+server.get('/favicon.*', (req, res) => {
   // res.sendFile('favicon.ico');
 });
 
 // Listen server
 server.listen(3001);
+
+// Execute a command (callback based)
+exec('ls -B', (err, stdout, stderr) => {
+  if (err) {
+    throw error;
+  }
+  console.log(stdout);
+});
