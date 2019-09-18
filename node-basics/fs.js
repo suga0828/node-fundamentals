f86d33c2e4807659a9f202d73484dcae3843b416
Alexander Sandoval <alexander.sf0828@gmail.com>
2019-09-18 13:58:30 -0500

    Show fs module Part2

--- node-basics/fs.js
+++ node-basics/fs.js
@@ -15,19 +15,18 @@ fs.writeFile(
           console.error(error);
         } else {
           console.log(file);
+          fs.rename('example.txt', 'tutorial/example.txt', error => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log('successfully renamed file');
+            }
+          });
         }
       });
     }
   }
 );
-
-fs.rename('example2.txt', 'example3.txt', error => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('successfully renamed file');
-  }
-});
 
 fs.appendFile('example3.txt', 'Some data being appendend', error => {
   if (error) {
@@ -42,5 +41,58 @@ fs.unlink('example3.txt', error => {
     console.error(error);
   } else {
     console.log('File successfully deleted');
+  }
+});
+
+fs.mkdir('tutorial', error => {
+  if (error) {
+    console.error(error);
+  } else {
+    // Create a file
+    fs.writeFile(
+      'tutorial/1.txt',
+      'this is a file created with fs module of Node.js',
+      err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('File successfully created');
+        }
+      }
+    );
+    fs.writeFile(
+      'tutorial/2.txt',
+      'this is a file created with fs module of Node.js',
+      err => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('File successfully created');
+        }
+      }
+    );
+  }
+});
+
+fs.readdir('tutorial', (err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
+    for (const file of files) {
+      fs.unlink(`./tutorial/${file}`, error => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(`${file} successfully deleted`);
+        }
+      });
+    }
+    fs.rmdir('tutorial', err => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('folder succesfully deleted');
+      }
+    });
   }
 });
